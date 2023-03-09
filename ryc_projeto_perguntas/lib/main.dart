7ae99cc7c0711cd783a0c8c79821f7ff86822c77
Renan Santos <renan.s.santos@accenture.com>
2023-03-09 16:22:55 -0300

    Trabalhando com map

--- ryc_projeto_perguntas/lib/main.dart
+++ ryc_projeto_perguntas/lib/main.dart
@@ -16,13 +16,26 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
+    final perguntas = [
+      {
+        "texto": 'Qual sua cor favorita?',
+        "respostas": ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        "texto": 'Qual seu animal favorito?',
+        "respostas": ['Coelho', 'Cobra', 'Elefante', 'Leão']
+      },
+      {
+        "texto": 'Qual seu instrutor favorito?',
+        "respostas": ['Maria', 'João', 'Leo', 'Pedro']
+      },
+    ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Perguntas')),
         body: Column(children: <Widget>[
-          Questao(perguntas[_perguntaSelecionada]),
+          Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
           Resposta('Resposta 1', _responder),
           Resposta('Resposta 2', _responder),
           Resposta('Resposta 3', _responder),
