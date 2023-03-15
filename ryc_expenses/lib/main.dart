f4d424a642fdebd91fd97fccc3c740610451ec9c
Renan Santos <renan.s.santos@accenture.com>
2023-03-15 11:11:03 -0300

    iniciando módulo 4 - expenses

---
+++ ryc_expenses/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+      ),
+      body: const Center(
+        child: Text('Tela inicial'),
+      ),
+    );
+  }
+}
